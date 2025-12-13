5ad8951fd557c6bae20359df3dc6474550d85dfa
Usyup <finder.ucup@gmail.com>
2025-12-13 19:51:56 +0700

    UCUP : Menambahkan kometar berupa sequence escape

--- javascript-dasar/data-type-and-variable/string.js
+++ javascript-dasar/data-type-and-variable/string.js
@@ -1,7 +1,14 @@
-document.writeln("STRING");
+document.writeln("\\STRING\\");
 document.writeln("<br/>");
-document.writeln("BIASANAYA");
+document.writeln("BIASANAYA\n");
 document.writeln("<br/>");
-document.writeln("MEMAKAI");
+document.writeln("\tMEMAKAI");
 document.writeln("<br/>");
-document.writeln("PETIK DUA");
+document.writeln("\"PETIK DUA\"");
+
+// ENSCAPE SEQUENCE
+// \n -> ENTER
+// \t -> TAB
+// \’ -> ‘
+// \” ->  “
+// \\ -> \
