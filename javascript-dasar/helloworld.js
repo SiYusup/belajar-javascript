1c6865ead61e50b0779e11a582115030f41c556e
Usyup <finder.ucup@gmail.com>
2025-12-13 19:25:37 +0700

    UCUP : Menambahkan komentar berupa materinya ya

--- javascript-dasar/helloworld.js
+++ javascript-dasar/helloworld.js
@@ -1,2 +1,17 @@
 console.log("Hello world")
 document.writeln("Hello world");
+
+// Ini merupakan single komentar atau oneline komentar
+/*
+* sedangkan
+* ini merupakan all line komentar atau
+* multi komentar
+ */
+
+/*
+Pada kode progam js ini
+tidak diwajibkan untuk menambahkan titik koma
+tetapi kalau anda memakai titik koma maka dapat
+diharapkan untuk memakainya sampai akhir
+karena akan berdampak pada saat membuat library atau memakainya
+ */
