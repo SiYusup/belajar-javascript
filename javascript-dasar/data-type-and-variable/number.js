c12a0ea25dc885548ecc5b85dbd4a43f60af80e0
Usyup <finder.ucup@gmail.com>
2025-12-13 19:37:58 +0700

    UCUP : Saya menambakan index.html dan menambahkan folder type-data-and-number

---
+++ javascript-dasar/data-type-and-variable/number.js
@@ -0,0 +1,8 @@
+// || TIPE DATA NUMBER
+// || Pada tipe data number ini merupakan tipe data yang bisa dimasukkan sebagai
+// || float maupun integer, olehkarena itu number berlaku pada semua angka
+
+
+document.writeln(100); // Tipe data integer
+document.writeln("<br/>")
+document.writeln(100.103); // Tipe data float
