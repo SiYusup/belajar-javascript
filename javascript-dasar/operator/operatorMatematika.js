1f49f0f393609ee60920252e9ee3de6333e6f51b
Usyup <finder.ucup@gmail.com>
2025-12-14 06:51:01 +0700

    UCUP : Menambahkan materi operator matematika

---
+++ javascript-dasar/operator/operatorMatematika.js
@@ -0,0 +1,36 @@
+// OPERATOR MATEMATIKA
+// +  -> Pertambahan
+// -  -> Pengurangan
+// *  -> Perkalian
+// ** -> Exponensial
+// /  -> Pembagian
+// %  -> Sisa Bagi
+
+let n1 = 10;
+let n2 = 6;
+let n3 = 5;
+let result;
+
+// PERTAMBAHAN
+result = n1 + n2;
+document.writeln(`${n1} + ${n2} = ${result} <br/>`);
+
+// PENGURANAGAN
+result = n2 - n3;
+document.writeln(`${n2} - ${n3} = ${result} <br/>`)
+
+// PERKALIAN
+result = n1 * n3;
+document.writeln(`${n1} + ${n3} = ${result} <br/>`)
+
+// EXPONENSIAL
+result = n1 ** n1
+document.writeln(`${n1} ** ${n1} = ${result} <br/>`)
+
+// PEMBAGIAN
+result = n1 / n3;
+document.writeln(` ${n2} / ${n3} = ${result} <br/>`)
+
+// SISA BAGI
+result = n2 % n3;
+document.writeln(`${n2} % ${n3} = ${result} <br/>`)
