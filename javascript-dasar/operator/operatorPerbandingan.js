e46f389d7c4aaa7b0aec61c3559439b2b7461408
Usyup <finder.ucup@gmail.com>
2025-12-14 07:07:58 +0700

    UCUP : Memnambahkan materi operator perbandingan

---
+++ javascript-dasar/operator/operatorPerbandingan.js
@@ -0,0 +1,36 @@
+// OPERATOR PERBANDINGAN
+// > Lebih Dari
+// < Kurang Dari
+// >= Lebih Dari Sama Dengan
+// <= Kurang Dari Sama Dengan
+// == Sama Dengan
+// === Sama Dengan dan Sama Tipe
+// != Tidak Sama Dengan
+// !== Tidak Sama Dengan atau Tidak Sama Tipe
+
+let result;
+
+result = 5 == "5";
+document.writeln(result + "<br/>")
+
+result = 5 === "5";
+document.writeln(result + "<br/>")
+
+result = 5 > 2;
+document.writeln(result + "<br/>")
+
+result = 5 >= 5;
+document.writeln(result + "<br/>")
+
+result = 5 < 1;
+document.writeln(result + "<br/>")
+
+result = 5 <= 5;
+document.writeln(result + "<br/>")
+
+result = 5 != 2;
+document.writeln(result + "<br/>")
+
+result = 5 !== "5";
+document.writeln(result + "<br/>")
+
